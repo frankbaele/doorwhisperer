f938a8de7de254dc7b3932956ccae034bc2f621d
Frank Baele <frankbaele@gmail.com>
2016-04-22 16:58:35 +0200

    fix fixy fix

--- src/services/dungeon.js
+++ src/services/dungeon.js
@@ -10,25 +10,25 @@ var _ = {
     clone: require('lodash.clone')
 };
 
-var roomsCount = 7;
+var roomsCount = 30;
 var dungeon;
 var startPos;
 var exitPos;
 var wandererPos;
-function generate(){
+function generate() {
     map = [];
     dungeon = new Dungeon(
         {
             "size": [100, 100],
             "rooms": {
                 "initial": {
-                    "min_size": [1,1],
-                    "max_size": [1,1],
+                    "min_size": [5, 5],
+                    "max_size": [5, 5],
                     "max_exits": 1
                 },
                 "any": {
-                    "min_size": [1,1],
-                    "max_size": [1,1],
+                    "min_size": [5, 5],
+                    "max_size": [5, 5],
                     "max_exits": 4
                 }
             },
@@ -36,40 +36,61 @@ function generate(){
             "min_corridor_length": 0,
             "corridor_density": 0,
             "symmetric_rooms": true,
-            "interconnects": 10,
-            "max_interconnect_length": 10,
+            "interconnects": 3,
+            "max_interconnect_length": 1,
             "room_count": roomsCount
         }
     );
 
     dungeon.generate();
+    dungeon.print();
+    // calculate maxs
+    var max = {
+        x: 0,
+        z: 0
+    };
+    _.forEach(dungeon.children, function (child) {
+        var pos = {
+            z: child.position[1] / 6,
+            x: child.position[0] / 6
+        };
 
-    for (var y = 0; y < dungeon.size[1]; y++) {
+        if (pos.z > max.z) {
+            max.z = pos.z
+        }
+        if (pos.x > max.x) {
+            max.x = pos.x
+        }
+    });
+    // generate grid based on max values
+    for (var i = 0; i <= max.z; i++) {
         var row = [];
-        for (var x = 0; x < dungeon.size[0]; x++) {
-            if(dungeon.walls.get([x, y])){
-                row.push(null);
-            }
-            else {
-                var textureIndex = libs.getRandomInt(0, CONST.wallTextures.length - 1);
-                row.push({
-                    texture: CONST.wallTextures[textureIndex]
-                });
-            }
+        for (var b = 0; b <= max.x; b++) {
+            row.push(null);
         }
         map.push(row);
     }
+    // populate the map with the rooms
+    _.forEach(dungeon.children, function(child){
+        var pos = {
+            z: child.position[1] / 6,
+            x: child.position[0] / 6
+        };
+        var textureIndex = libs.getRandomInt(0, CONST.wallTextures.length - 1);
+        map[pos.z][pos.x] = {
+            texture: CONST.wallTextures[textureIndex]
+        }
+    });
 
     startPos = {
-        z: dungeon.start_pos[1],
-        x: dungeon.start_pos[0]
+        z: dungeon.children[0].position[1]/ 6,
+        x: dungeon.children[0].position[0]/ 6
     };
-
     map[startPos.z][startPos.x].texture = 'img/walls/nether_brick.png';
     var lastRoom = dungeon.children[dungeon.children.length - 1];
     exitPos = {
-        z: lastRoom.position[1] + 1,
-        x: lastRoom.position[0] + 1
+        z: lastRoom.position[1]/ 6,
+        x: lastRoom.position[0]/ 6
     };
 
     map[exitPos.z][exitPos.x].type = "win";
@@ -78,8 +99,8 @@ function generate(){
     var wandererRoom = dungeon.children[libs.getRandomInt(5, roomsCount - 1)];
 
     wandererPos = {
-        z: wandererRoom.position[1] + 1,
-        x: wandererRoom.position[0] + 1
+        z: wandererRoom.position[1]/ 6,
+        x: wandererRoom.position[0]/ 6
     };
 
     var rooms = _.clone(dungeon.children);
@@ -88,15 +109,15 @@ function generate(){
     rooms.pop();
 
     // we only want rooms with one exit, so we never block the player in a passage room
-    rooms = _.filter(rooms, function(room){
+    rooms = _.filter(rooms, function (room) {
         return room.exits.length == 1;
     });
 
-    _.forEach(rooms, function(room){
+    _.forEach(rooms, function (room) {
         var roomTypeIndex = libs.getRandomInt(0, roomTypes.length - 1);
         var roomPos = {
-            z: room.position[1] + 1,
-            x: room.position[0] + 1
+            z: room.position[1]/ 6,
+            x: room.position[0]/ 6
         };
         map[roomPos.z][roomPos.x] = _.clone(roomTypes[roomTypeIndex]);
     });
@@ -104,19 +125,19 @@ function generate(){
 
 module.exports = {
 
-    map: function(){
+    map: function () {
         return map;
     },
-    generate: function(){
+    generate: function () {
         generate();
     },
-    startPos: function(){
+    startPos: function () {
         return startPos;
     },
-    exitPos: function(){
+    exitPos: function () {
         return exitPos;
     },
-    wandererPos: function(){
+    wandererPos: function () {
         return wandererPos;
     }
 };
